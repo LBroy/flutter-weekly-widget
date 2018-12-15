3a013706a780f1717937f874e041f25212b9ceb7
roylaw <roylaw.w@gmail.com>
2018-12-15 16:43:26 -0500

    Animation Left to Right
    SingleTickerProvider, AnimationController, Animation, Transform etc are used to animate a colored box moved from left to right in the screen

---
+++ ltr_animation/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: "left to right animation",
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 7),
+    );
+    _animation = Tween(begin: -1.0, end: 1.0).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut))
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _animationController.reset();
+        } else if (status == AnimationStatus.dismissed) {
+          // _animationController.forward();
+        }
+      });
+    // _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animation Left to Right"),
+      ),
+      body: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) => Transform(
+              transform: Matrix4.translationValues(
+                  _animation.value * screenWidth, 0.0, 0.0),
+              child: Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+      ),
+    );
+  }
+}
