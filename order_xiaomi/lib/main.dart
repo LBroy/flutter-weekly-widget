78635f8bc6af23674087c311daacbbaff5be9895
roylaw <roylaw.w@gmail.com>
2018-12-19 17:24:02 -0500

    OrderPage Completed
    Stack is great, SizedBox is great.
    Question:
    1, How to overrite specific widget’s padding or any properties so it won’t use the inherited values?
    2, How to change background of the whole body in Scaffold?

--- order_xiaomi/lib/main.dart
+++ order_xiaomi/lib/main.dart
@@ -23,49 +23,69 @@ class _OrderPageState extends State<OrderPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              "My Orders",
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.w900,
+        child: Stack(children: [
+          ListView(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "My Orders",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w900,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              height: 35,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  OrderChoiceChip(
-                    label: "All",
-                    selected: false,
-                  ),
-                  OrderChoiceChip(
-                    label: "Paid",
-                    selected: true,
-                  ),
-                  OrderChoiceChip(
-                    label: "Shipped",
-                    selected: false,
-                  ),
-                  OrderChoiceChip(
-                    label: "Returned",
-                    selected: false,
-                  ),
-                ],
+              SizedBox(
+                height: 20.0,
               ),
+              Container(
+                  height: 35,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      OrderChoiceChip(
+                        label: "All",
+                        selected: false,
+                      ),
+                      OrderChoiceChip(
+                        label: "Paid",
+                        selected: true,
+                      ),
+                      OrderChoiceChip(
+                        label: "Shipped",
+                        selected: false,
+                      ),
+                      OrderChoiceChip(
+                        label: "Returned",
+                        selected: false,
+                      ),
+                    ],
+                  )),
+              OrderCard(
+                orderId: "860368",
+              ),
+              OrderCard(
+                orderId: "980368",
+              ),
+              SizedBox(
+                height: 100,
+              )
+            ],
+          ),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            right: 0,
+            height: 100,
+            child: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [Colors.white, Colors.white.withOpacity(0.1)],
+              )),
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            OrderCard(),
-          ],
-        ),
+          )
+        ]),
       ),
     );
   }
--- order_xiaomi/lib/orderCard.dart
+++ order_xiaomi/lib/orderCard.dart
@@ -1,50 +1,106 @@
 import 'package:flutter/material.dart';
 
 class OrderCard extends StatelessWidget {
+  final String orderId;
+  OrderCard({this.orderId});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          "Order ID #860368",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Order ID #$orderId",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        Column(
-          children: <Widget>[
-            ProductCard(
-              productImageUrl:
-                  "https://image2.geekbuying.com/ggo_pic/2018-10-25/Xiaomi-Mi-Mix-3-6-39-Inch-6GB-128GB-Smartphone-Black-757111-.jpg",
-              productName: "Xiaomi Mi Mix 3",
-              productPrice: 160,
-              productColor: "Slate Black",
-              productQuantity: 1,
-              productStatus: "Packing",
-            ),
-            ProductCard(
-              productImageUrl:
-                  "https://cdn.eglobalcentraluk.com/images/detailed/69/xiaomi-mi-8-6gb-128gb-dual-sim-black.jpg",
-              productName: "Xiaomi A2 Lite",
-              productPrice: 100,
-              productColor: "Black",
-              productQuantity: 1,
-              productStatus: "Processing",
-            ),
-            ButtonBar(
-              children: <Widget>[
-                FlatButton(),
-                RaisedButton(),
-              ],
-            )
-          ],
-        ),
-      ],
+          SizedBox(
+            height: 20.0,
+          ),
+          Column(
+            children: <Widget>[
+              ProductCard(
+                productImageUrl:
+                    "https://image2.geekbuying.com/ggo_pic/2018-10-25/Xiaomi-Mi-Mix-3-6-39-Inch-6GB-128GB-Smartphone-Black-757111-.jpg",
+                productName: "Xiaomi Mi Mix 3",
+                productPrice: 160,
+                productColor: "Slate Black",
+                productQuantity: 1,
+                productStatus: "Packing",
+              ),
+              ProductCard(
+                productImageUrl:
+                    "https://cdn.eglobalcentraluk.com/images/detailed/69/xiaomi-mi-8-6gb-128gb-dual-sim-black.jpg",
+                productName: "Xiaomi A2 Lite",
+                productPrice: 100,
+                productColor: "Black",
+                productQuantity: 1,
+                productStatus: "Processing",
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Stack(children: [
+                    FlatButton(
+                      padding: EdgeInsets.all(0.0),
+                      onPressed: () {},
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 25.0, vertical: 10.0),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            border: Border.all(width: 2.0, color: Colors.blue)),
+                        child: Text(
+                          "Messages (2)",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      top: 2,
+                      child: Container(
+                        height: 8.0,
+                        width: 8.0,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.red),
+                      ),
+                    )
+                  ]),
+                  FlatButton(
+                    padding: EdgeInsets.all(0.0),
+                    onPressed: () {},
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 27.0, vertical: 12.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          gradient: LinearGradient(
+                            colors: [Colors.blue, Colors.green],
+                          )),
+                      child: Text(
+                        "Order Details",
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -65,12 +121,12 @@ class ProductCard extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 20.0),
       elevation: 8.0,
       child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 20.0),
+        padding: EdgeInsets.symmetric(vertical: 10.0),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
               child: Image.network(
                 productImageUrl,
                 height: 120,
